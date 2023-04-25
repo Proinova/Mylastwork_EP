6f39249676a9b61ad9822c09e288086c5e35adc4
Eli <elisaveta.proinova@gmail.com>
2023-04-25 15:26:32 +0300

    25.04

---
+++ database.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS spain;
+CREATE DATABASE spain;
+USE spain;
+CREATE TABLE potrebiteli(
+ id INT,
+ name VARCHAR(255),
+ pass VARCHAR(255),
+ mail VARCHAR(255)
+);
+INSERT INTO potrebiteli VALUES
+ (1,"admin","admin","admin@local.bg");

ccfe027d780b05c7f060ac93c5c50ffe4b6826a6
Eli <elisaveta.proinova@gmail.com>
2023-04-28 11:24:49 +0300

    potrebiteli

--- src/java/RegisterServlet.java
+++ src/java/RegisterServlet.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,6 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,10 +30,19 @@ public class RegisterServlet extends HttpServlet {
                </html>
                """;
         
-          String db = "jdbc:mariadb://localhost/andalusia";
+        String db = "jdbc:mariadb://localhost/spain";
+        String reqName;
+        String reqPass;
+        String reqMail;
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             Connection conn = DriverManager.getConnection(db);
+            Statement stmt = conn.createStatement();
+            String query = """
+                    INSERT INTO potrebiteli(name, pass, mail)
+                    VALUES (\"el\", \"i\", \"pr\");
+                           """;
+            ResultSet rs = stmt.executeQuery(query);
         } catch (SQLException ex) {
             Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
